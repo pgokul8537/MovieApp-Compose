fede5a209bbbbaaf2be7bd9a830f7376126dbccf
Gokul P <Gokul.p@way.com>
2023-12-06 19:07:49 +0530

    removed token

--- app/src/main/java/com/example/movieapp/utils/Constants.kt
+++ app/src/main/java/com/example/movieapp/utils/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val imageUrl: String = "https://image.tmdb.org/t/p/w500"
     const val originalImageUrl: String = "https://image.tmdb.org/t/p/original"
     const val access_token: String =
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWY5ZmYzMzhlNGY4MTc2Y2Q1OTM3MDZiODJmNTAxMSIsInN1YiI6IjVkYzk1ZmQ3NDcwZWFkMDAxMzk4N2ZhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4q29og1uwwWqxFp63WK2W3qM8Wh0ZOq_N_waEmhkh2U"
+        "Bearer Token"
 
     //    Movies
     const val URL_POPULAR_MOVIES: String = "movie/popular"
